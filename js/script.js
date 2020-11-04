ef9aed114d8f773926f910d14529f2e2b0f30d94
Sergii Grynevych <grinevich.sergey.official@gmail.com>
2020-11-04 17:35:15 +0200

    all tasks are done

--- js/script.js
+++ js/script.js
@@ -12,8 +12,6 @@
 
 5) Добавить нумерацию выведенных фильмов */
 
-'use strict';
-
 const movieDB = {
     movies: [
         "Логан",
@@ -25,7 +23,9 @@ const movieDB = {
 };
 
 // 1) Удаляет все рекламные блоки со страницы (правая часть сайта)
-document.querySelector('.promo__adv').remove('.promo__adv');
+const promoImg = document.querySelectorAll('.promo__adv img');
+
+promoImg.forEach((item) => item.remove());
 
 // 2) Изменить жанр фильма, поменять "комедия" на "драма"
 const changeElByClass = document.querySelector('.promo__genre');
@@ -33,9 +33,30 @@ changeElByClass.textContent = 'драма';
 
 // 3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
 // Реализовать только при помощи JS
+const editBackground = document.querySelector('.promo__bg');
 
+editBackground.style = 'background-image: url(../img/bg.jpg)';
 
+// 4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+// Отсортировать их по алфавиту 
 
+const promoInteractiveItem = document.querySelector('.promo__interactive-list');
+
+promoInteractiveItem.innerHTML = '';
+
+const sortedFilms = movieDB.movies.map((item) => item).sort(); 
+// можно сделать вместо 47 строчки:
+// movieDB.movies.sort();
+// и в 52 строчку вместо sortedFilms вписать movieDB.movies
+
+sortedFilms.forEach((film, ind) => {
+    promoInteractiveItem.innerHTML +=
+        `<li class="promo__interactive-item">${ind + 1} ${film}
+            <div class="delete"></div>
+         </li>`;
+});
+
+console.log(promoInteractiveItem);
 
 
 
