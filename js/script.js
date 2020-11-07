1aac1ee2e3c74628c786e68a66e92b5b41965d39
Sergii Grynevych <grinevich.sergey.official@gmail.com>
2020-11-07 14:48:16 +0200

    all tasks are done. When user add film into input, this film you may see at the list of favourite films

--- js/script.js
+++ js/script.js
@@ -1,62 +1,94 @@
-/* Задания на урок:
+document.addEventListener('DOMContentLoaded', () => {
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
 
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
+    const promoImg = document.querySelectorAll('.promo__adv img');
+    const changeElByClass = document.querySelector('.promo__genre');
+    const editBackground = document.querySelector('.promo__bg');
+    const promoInteractiveItem = document.querySelector('.promo__interactive-list');
+    const form = document.querySelector('form.add');
+    const input = form.querySelector('.adding__input');
+    const checkbox = form.querySelector('[type = "checkbox"]');
 
-2) Изменить жанр фильма, поменять "комедия" на "драма"
+    form.addEventListener('submit', (event) => {
+        // убираю перезагрузку страницы после нажания на "подтвердить"
+        event.preventDefault();
 
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
+        let newFilm = input.value;
+        const favouriteFilms = checkbox.checked;
 
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
+        if (newFilm) {
+            // Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
+            if (newFilm.length > 21) {
+                newFilm = `${newFilm.substring(0, 22)}...`;
+            }
+            // Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение:
+            //"Добавляем любимый фильм"
+            if (favouriteFilms) {
+                console.log("Добавляем любимый фильм");
+            }
+            // пушу введенный пользователем фильм в массив с всеми фильмами 
+            // и перевожу первую букву в верхний регистр, а все остальные в нижний регистр
+            movieDB.movies.push(newFilm[0].toUpperCase() + newFilm.slice(1).toLowerCase());
+            //вызываю callback function сортировки всего массива с фильмами
+            sortFilms(movieDB.movies);
+            //вызываю callback function для создания разметки под елемент и отображения его на странице 
+            addFilmList(movieDB.movies, promoInteractiveItem);
+        }
+        //сбрасываю placeholder после нажатия кнопки "подтвердить"
+        form.reset();
+    });
 
-5) Добавить нумерацию выведенных фильмов */
+    // 1) Удаляет все рекламные блоки со страницы (правая часть сайта)
+    const removeImgPromo = (arr) => {
+            arr.forEach(item => item.remove());
+    };
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
+    const changeBcgAndTextContent = () => {
+        // 2) Изменить жанр фильма, поменять "комедия" на "драма"
+        changeElByClass.textContent = 'драма';
+        // 3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+        // Реализовать только при помощи JS
+        editBackground.style.backgroundImage = 'url(img/bg.jpg)';
+    };
+    // 4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+    // Отсортировать их по алфавиту 
+    const sortFilms = (movies) => {
+        movies.sort();
+    };
 
-// 1) Удаляет все рекламные блоки со страницы (правая часть сайта)
-const promoImg = document.querySelectorAll('.promo__adv img');
+    const addFilmList = (film, parent) => {
+        parent.innerHTML = '';
+        sortFilms(film);
 
-promoImg.forEach((item) => item.remove());
+        film.forEach((film, ind) => {
+            promoInteractiveItem.innerHTML +=
+                `<li class="promo__interactive-item">${ind + 1} ${film}
+                        <div class="delete"></div>
+                    </li>`;
+        });
 
-// 2) Изменить жанр фильма, поменять "комедия" на "драма"
-const changeElByClass = document.querySelector('.promo__genre');
-changeElByClass.textContent = 'драма';
+        document.querySelectorAll('.delete').forEach((btn, i) => {
+            btn.addEventListener('click', () => {
+                btn.parentElement.remove();
+                movieDB.movies.splice(i, 1);
+                
+                addFilmList(film, parent);
+            });
+        });
+    };
 
-// 3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-// Реализовать только при помощи JS
-const editBackground = document.querySelector('.promo__bg');
-
-editBackground.style.backgroundImage = 'url(img/bg.jpg)';
-
-// 4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-// Отсортировать их по алфавиту 
-
-const promoInteractiveItem = document.querySelector('.promo__interactive-list');
-
-promoInteractiveItem.innerHTML = '';
-
-const sortedFilms = movieDB.movies.map((item) => item).sort(); 
-// можно сделать вместо 47 строчки:
-// movieDB.movies.sort();
-// и в 52 строчку вместо sortedFilms вписать movieDB.movies
-
-sortedFilms.forEach((film, ind) => {
-    promoInteractiveItem.innerHTML +=
-        `<li class="promo__interactive-item">${ind + 1} ${film}
-            <div class="delete"></div>
-         </li>`;
+    removeImgPromo(promoImg);
+    changeBcgAndTextContent();
+    addFilmList(movieDB.movies, promoInteractiveItem);
 });
-
-console.log(promoInteractiveItem);
 
 
 
